448438f0fe4c12244e313fff5a5228ed54c43ae0
{GluCmA$ch1n3} <KosAlexxx.kosalexxx@gmail.com>
2023-12-01 11:11:13 +0300

    REQ-03: Email, firstName, lastName validation

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -9,11 +9,12 @@ public class Main {
     static String PASSWORD = "default";
     static Scanner SCANNER = new Scanner(System.in);
     static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    static String FIRST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
+    static String LAST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
 
     public static void main(String[] args) {
-        if(auth()){
-            registerNewClient();
-        }
+        run();
+
     }
 
     private static void registerNewClient() {
@@ -21,22 +22,40 @@ public class Main {
         System.out.print("email: ");
         String email = SCANNER.nextLine();
 
-        if(isEmailValid(email)){
-            Client client = buildClient(email);
+        if(isEmailValid(email)) {
+            String validEmail = email;
+
+            System.out.println("FirstName: ");
+            String firstName = SCANNER.nextLine();
+
+            Client client = null;
+            if (isFirstNameValid(firstName)) {
+                String validFirstName = firstName;
+
+                System.out.println("LastName: ");
+                String LastName = SCANNER.nextLine();
+
+                if (isLastNameValid(firstName)) {
+                    String validLastName = LastName;
+                    client = buildClient(validEmail, validFirstName, validLastName);
+                } else {
+                    System.out.println("Provided firstName is invalid.");
+                }
+            } else {
+                System.out.println("Provided firstName is invalid.");
+            }
+
             System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
         } else {
             System.out.println("Provided email is invalid.");
         }
     }
 
-    private static Client buildClient(String email) {
+    private static Client buildClient(String email, String firstName, String lastName) {
         Client client = new Client();
         client.email = email;
-
-        System.out.println("FirstName: ");
-        client.firstName = SCANNER.nextLine();
-        System.out.println("lastName: ");
-        client.lastName = SCANNER.nextLine();
+        client.firstName = firstName;
+        client.lastName = lastName;
         return client;
     }
 
@@ -46,8 +65,22 @@ public class Main {
         return matcher.matches();
     }
 
-    private static void run() {
+    private static boolean isFirstNameValid(String firstName){
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 
+    private static boolean isLastNameValid(String lastName){
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    private static void run() {
+        if(auth()){
+            registerNewClient();
+        }
     }
 
     static boolean auth(){
