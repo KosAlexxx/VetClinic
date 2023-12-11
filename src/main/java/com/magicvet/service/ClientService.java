4183d128a7bb1d2874eecb375c7d00dc3a92893e
{GluCmA$ch1n3} <KosAlexxx.kosalexxx@gmail.com>
2023-12-11 12:05:31 +0300

    REQ-05: Creat util package and class Validator

--- src/main/java/com/magicvet/service/ClientService.java
+++ src/main/java/com/magicvet/service/ClientService.java
@@ -2,14 +2,9 @@ package main.java.com.magicvet.service;
 
 import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Client;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import main.java.com.magicvet.utils.Validator;
 
 public class ClientService {
-
-   private static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-   private static String FIRST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
-   private static String LAST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
 
     public Client registerNewClient() {
         Client client = null;
@@ -17,15 +12,15 @@ public class ClientService {
         System.out.print("email: ");
         String email = Main.SCANNER.nextLine();
 
-        if(isEmailValid(email)) {
+        if(Validator.isEmailValid(email)) {
             System.out.println("FirstName: ");
             String firstName = Main.SCANNER.nextLine();
 
-            if (isFirstNameValid(firstName)) {
+            if (Validator.isFirstNameValid(firstName)) {
                 System.out.println("LastName: ");
                 String lastName = Main.SCANNER.nextLine();
 
-                if (isLastNameValid(lastName)) {
+                if (Validator.isLastNameValid(lastName)) {
                     client = buildClient(email, firstName, lastName);
 
                 } else {
@@ -54,22 +49,6 @@ public class ClientService {
         return client;
     }
 
-    private static boolean isEmailValid(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
 
-    private static boolean isFirstNameValid(String firstName){
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        Matcher matcher = pattern.matcher(firstName);
-        return matcher.matches();
-    }
-
-    private static boolean isLastNameValid(String lastName){
-        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
-        Matcher matcher = pattern.matcher(lastName);
-        return matcher.matches();
-    }
 
 }
---
+++ src/main/java/com/magicvet/utils/Validator.java
@@ -0,0 +1,30 @@
+package main.java.com.magicvet.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    private static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static String FIRST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
+    private static String LAST_NAME_PATTERN = "^[A-zА-я]{1,}[- ]?[A-zА-я]{2,}$";
+
+    public static boolean isEmailValid(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isFirstNameValid(String firstName){
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    public static boolean isLastNameValid(String lastName){
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+}
