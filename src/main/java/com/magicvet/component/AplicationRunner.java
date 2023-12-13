94c998cf297646e6706813bc447c55af32dae724
{GluCmA$ch1n3} <KosAlexxx.kosalexxx@gmail.com>
2023-12-13 18:52:33 +0300

    REQ-06: Creat util package and class Validator and add Pet

--- src/main/java/com/magicvet/component/AplicationRunner.java
+++ src/main/java/com/magicvet/component/AplicationRunner.java
@@ -1,5 +1,6 @@
 package main.java.com.magicvet.component;
 
+import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Client;
 import main.java.com.magicvet.model.Pet;
 import main.java.com.magicvet.service.ClientService;
@@ -14,11 +15,16 @@ public class AplicationRunner {
          Client client = clientService.registerNewClient();
 
          if(client != null){
-             System.out.println("Adding a new pet");
-             Pet pet = petService.registerNewPet();
-             client.setPet(pet);
-             pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-             System.out.println("Pet has been added.");
+             System.out.println("Do you want add a pet (y/n)");
+
+             if(Main.SCANNER.nextLine().equals("y")) {
+
+                 System.out.println("Adding a new pet");
+                 Pet pet = petService.registerNewPet();
+                 client.setPet(pet);
+                 pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+                 System.out.println("Pet has been added.");
+             }
          }
         }
     }
